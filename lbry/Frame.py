d6465761e867156b04792a4c9224c70ba9a289d4
Peng5653 <mysky5653@gmail.com>
2020-09-07 15:54:39 +0900

    Fix: 데이터 셀 값 수정
    데이터를 불러오는 셀 값 입력을 수정함.
    또한 각 제조사 시트를 불러 올 수 있도록 구조를 수정함

--- lbry/Frame.py
+++ lbry/Frame.py
@@ -15,24 +15,40 @@ class ShipFrame:
     
     def shipInfo(self):
         if(self._MFT == 'RSI' and self._name == 'ES'):
+            _mft = load_RSI
             _num = 3
 
+        if(self._MFT == 'RSI' and self._name == 'MR'):
+            _mft = load_RSI
+            _num = 4
+
+        if(self._MFT == 'RSI' and self._name == 'CL'):
+            _mft = load_RSI
+            _num = 5
+        
+        if(self._MFT == 'RSI' and self._name == 'LN'):
+            _mft = load_RSI
+            _num = 6
+
+        if(self._MFT == 'RSI' and self._name == 'LX'):
+            _mft = load_RSI
+            _num = 7
         
 
         embed = discord.Embed(
-            title=(load_RSI.cell(_num,_num-1).value), 
-            description=(load_RSI.cell(_num,_num+9).value), 
+            title=(_mft.cell(_num,3-1).value), 
+            description=(_mft.cell(_num,3+9).value), 
             colour = 00000000)
         embed.set_author(name= self._title)
-        embed.add_field(name= "제조사", value=(load_RSI.cell(_num,_num).value), inline= True)
-        embed.add_field(name= "구현단계", value=(load_RSI.cell(_num,_num+1).value), inline= False)
-        embed.add_field(name= "전장/전폭/전고", value=(load_RSI.cell(_num,_num+2).value), inline= True)
-        embed.add_field(name= "최소/최대 승무원", value=(load_RSI.cell(_num,_num+3).value), inline= True)
-        embed.add_field(name= "순항/최대 속도", value=(load_RSI.cell(_num,_num+4).value), inline= True)
-        embed.add_field(name= "분류", value=(load_RSI.cell(_num,_num+5).value), inline= True)
-        embed.add_field(name= "출고중량", value=(load_RSI.cell(_num,_num+6).value), inline= True)
-        embed.add_field(name= "화물용적", value=(load_RSI.cell(_num,_num+7).value), inline= True)
-        embed.add_field(name= "가격($)", value=(load_RSI.cell(_num,_num+8).value), inline= True)
+        embed.add_field(name= "제조사", value=(_mft.cell(_num,3).value), inline= True)
+        embed.add_field(name= "구현단계", value=(_mft.cell(_num,3+1).value), inline= False)
+        embed.add_field(name= "전장/전폭/전고", value=(_mft.cell(_num,3+2).value), inline= True)
+        embed.add_field(name= "최소/최대 승무원", value=(_mft.cell(_num,3+3).value), inline= True)
+        embed.add_field(name= "순항/최대 속도", value=(_mft.cell(_num,3+4).value), inline= True)
+        embed.add_field(name= "분류", value=(_mft.cell(_num,3+5).value), inline= True)
+        embed.add_field(name= "출고중량", value=(_mft.cell(_num,3+6).value), inline= True)
+        embed.add_field(name= "화물용적", value=(_mft.cell(_num,3+7).value), inline= True)
+        embed.add_field(name= "가격($)", value=(_mft.cell(_num,3+8).value), inline= True)
         embed.set_image(url= self._image)
         embed.set_footer(text= self._footer)
         return embed
