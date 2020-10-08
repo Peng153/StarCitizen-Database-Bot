2b4704117b0410dacc06947902685c4a22b2645d
Peng5653 <mysky5653@gmail.com>
2020-10-08 22:25:42 +0900

    Feat: 명령어 추가 및 구조변경
    Ship.py 모듈 연결
    해당 모듈에서 각 함수를 불러옴
    아폴로 트리아지 명령 추가
    아폴로 메디벡 명령 추가

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 
 from lbry.Frame import ShipFrame
+from lbry.Ship import RSI, ANVL, CRUS
 
 bot = commands.Bot(command_prefix="!")
 bot.remove_command("help")
 
 # 토큰 입력
-token = "NzEwMzUxNDQyOTUwNzUwMjgx.XrzMVQ.etzGv5bLbLBTK5BRcUE2Yi59nVk"
+token = ""
 
 
 @bot.event
@@ -68,87 +69,61 @@ async def ship(context, name=None):
         embed.set_footer(text= _footer)
         await context.send(embed=embed)
 
+
+
     # Help RSI
     if name=="help_RSI":
-        embed = discord.Embed(
-            title="도움말", 
-            description="Roberts Space Industries 항목의 명령어 목록입니다. 대문자와 띄어쓰기에 주의 해 주시길 바랍니다.", 
-            colour = 00000000)
-        embed.add_field(name= "오로라ES", value="Auroraes, AuroraES, 오로라es, 오로라ES, 오로랃ㄴ, 오로라깡통", inline= False)
-        embed.add_field(name= "오로라MR", value="Auroramr, AuroraMR, 오로라mr, 오로라MR, 오로라ㅡㄱ", inline= False)
-        embed.add_field(name= "오로라CL", value="Auroracl, AuroraCL, 오로라cl, 오로라CL, 오로라치", inline= False)
-        embed.add_field(name= "오로라LN", value="Auroraln, AuroraLN, 오로라ln, 오로라LN, 오로라ㅣㅜ", inline= False)
-        embed.add_field(name= "오로라LX", value="Auroralx, AuroraLX, 오로라lx, 오로라LX, 오로라ㅣㅌ", inline= False)
-        embed.set_author(name= _title)
-        embed.set_footer(text= _footer)
+        embed = RSI(_title, _footer).Help_RSI() # lbry/Ship -> RSI Class -> Help_RSI
         await context.send(embed=embed)
 
     # Help ANVL
     if name=="help_ANVL":
-        embed = discord.Embed(
-            title="도움말", 
-            description="Anvil Aerospace 항목의 명령어 목록입니다. 대문자와 띄어쓰기에 주의 해 주시길 바랍니다.", 
-            colour = 00000000)
-        embed.add_field(name= "테스트", value="명령어 목록", inline= False)
-        embed.set_author(name= _title)
-        embed.set_footer(text= _footer)
+        embed = ANVL(_title, _footer).Help_ANVL() # lbry/Ship -> ANVL Class -> Help_ANVL
         await context.send(embed=embed)
 
     # Help CRUS
     if name=="help_CRUS":
-        embed = discord.Embed(
-            title="도움말", 
-            description="Crusader Industries 항목의 명령어 목록입니다. 대문자와 띄어쓰기에 주의 해 주시길 바랍니다.", 
-            colour = 00000000)
-        embed.add_field(name= "테스트", value="명령어 목록", inline= False)
-        embed.set_author(name= _title)
-        embed.set_footer(text= _footer)
+        embed = CRUS(_title, _footer).Help_CRUS() # lbry/Ship -> CRUS Class -> Help_CRUS
         await context.send(embed=embed)
+
+
 
     # Aurora_ES
     if name=="Auroraes" or name=="AuroraES" or name=="오로라es" or name=="오로라ES" or name=="오로랃ㄴ" or name=="오로라깡통":
-        _MFT = 'RSI'    # 제조사
-        _name = 'ES'    # 기종
-        _image = 'https://starcitizen.tools/images/8/8d/Aurora_ES_in_SelfLand_-_Isometric.jpg'
-
-        embed = ShipFrame(_title, _footer, _MFT, _name, _image).shipInfo()
+        embed = RSI(_title, _footer).Aurora_ES() # lbry/Ship -> RSI Class -> Aurora_ES
         await context.send(embed=embed)
 
     # Aurora_MR
     if name=="Auroramr" or name=="AuroraMR" or name=="오로라mr" or name=="오로라MR" or name=="오로라ㅡㄱ":
-        _MFT = 'RSI'    # 제조사
-        _name = 'MR'    # 기종
-        _image = 'https://starcitizen.tools/images/c/c4/Aurora_MR_in_SelfLand_-_Isometric.jpg'
-
-        embed = ShipFrame(_title, _footer, _MFT, _name, _image).shipInfo()
+        embed = RSI(_title, _footer).Aurora_MR() # lbry/Ship -> RSI Class -> Aurora_MR
         await context.send(embed=embed)
 
     # Aurora_CL
     if name=="Auroracl" or name=="AuroraCL" or name=="오로라cl" or name=="오로라CL" or name=="오로라치":
-        _MFT = 'RSI'    # 제조사
-        _name = 'CL'    # 기종
-        _image = 'https://starcitizen.tools/images/3/37/Aurora_CL_in_SelfLand_-_Isometric.jpg'
-
-        embed = ShipFrame(_title, _footer, _MFT, _name, _image).shipInfo()
+        embed = RSI(_title, _footer).Aurora_CL() # lbry/Ship -> RSI Class -> Aurora_CL
         await context.send(embed=embed)
 
     # Aurora_LN
     if name=="Auroraln" or name=="AuroraLN" or name=="오로라ln" or name=="오로라LN" or name=="오로라ㅣㅜ":
-        _MFT = 'RSI'    # 제조사
-        _name = 'LN'    # 기종
-        _image = 'https://starcitizen.tools/images/6/6c/Aurora_LN_in_SelfLand_-_Isometric.jpg'
-
-        embed = ShipFrame(_title, _footer, _MFT, _name, _image).shipInfo()
+        embed = RSI(_title, _footer).Aurora_LN() # lbry/Ship -> RSI Class -> Aurora_LN
         await context.send(embed=embed)
 
     # Aurora_LX
     if name=="Auroralx" or name=="AuroraLX" or name=="오로라lx" or name=="오로라LX" or name=="오로라ㅣㅌ":
-        _MFT = 'RSI'    # 제조사
-        _name = 'LX'    # 기종
-        _image = 'https://starcitizen.tools/images/9/94/Aurora_LX_in_SelfLand_-_Isometric.jpg'
-
-        embed = ShipFrame(_title, _footer, _MFT, _name, _image).shipInfo()
+        embed = RSI(_title, _footer).Aurora_LX() # lbry/Ship -> RSI Class -> Aurora_LX
         await context.send(embed=embed)
+
+    # Apollo Triage
+    if name=="Apollotriage" or name=="ApolloTriage" or name=="아폴로트리아지" or name=="빨간아폴로" or name=="붉은병원선":
+        embed = RSI(_title, _footer).Apollo_Tri() # lbry/Ship -> RSI Class -> Apollo_Tri
+        await context.send(embed=embed)
+
+    # Apollo Medivac
+    if name=="Apollomedivac" or name=="ApolloMedivac" or name=="아폴로메디벡" or name=="흰아폴로" or name=="하얀병원선":
+        embed = RSI(_title, _footer).Apollo_Med() # lbry/Ship -> RSI Class -> Apollo_Med
+        await context.send(embed=embed)
+
+
 
     # Arrow
     if name=="애로우" or name=="애로롱":
@@ -170,7 +145,5 @@ async def ship(context, name=None):
         await context.send(embed=embed)
 
         
-
-    
 
 bot.run(token)
